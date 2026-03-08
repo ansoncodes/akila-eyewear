fb3447b7ba358a8c5afe62c89a5f35e3cf54de46
M A Anson <ansonantony783@gmail.com>
2026-03-08 10:54:23 +0530

    chore: bootstrap Django project and core settings

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+﻿from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("accounts.urls")),
+    path("api/", include("products.urls")),
+    path("api/", include("cart.urls")),
+    path("api/", include("orders.urls")),
+    path("api/", include("reviews.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
