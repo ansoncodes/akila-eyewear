fd290ace32b00c8eebb4e123668eeb5197f589bf
M A Anson <ansonantony783@gmail.com>
2026-04-10 19:29:28 +0530

    update

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.http import JsonResponse
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+
+def health(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
+    path("health/", health),
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
     path("api/", include("products.urls")),
@@ -15,9 +23,6 @@ urlpatterns = [
 ]
 
 # Serve media files in production for Render's ephemeral disk (Since seed_data regenerates them on deploy)
-from django.views.static import serve
-from django.urls import re_path
-
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
